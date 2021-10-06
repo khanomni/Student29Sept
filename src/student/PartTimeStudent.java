a30708cb6c4d66fd0e509c5469ada9dafa60a847
amna khan <khan453@sheridancollege.ca>
2021-10-06 18:27:20 -0400

    Adding new class called Part Time Student

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author amna-
+ */
+public class PartTimeStudent extends Student {
+    private int numOfCourses;
+
+    public int getNumOfStudent
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    }
+            
+}
