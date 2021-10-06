cc60ef2cfae9ce61e41be00c4b233c1cbeed8d3a
amna khan <khan453@sheridancollege.ca>
2021-10-06 18:51:28 -0400

    Adding new classes called Full Time Student

---
+++ src/student/FullTimeStudent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author amna-
+ */
+public class FullTimeStudent extends Student {
+    private int numSemesters;
+
+    public int getNumSemesters() {
+        return numSemesters;
+    }
+
+    public void setNumSemesters(int numSemesters) {
+        this.numSemesters = numSemesters;
+    }
+    
+}
